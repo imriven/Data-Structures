c0151d1992626f492b7af3f616920930c33c317d
Christina Harris <christinathedesigner@outlook.com>
2020-06-08 08:23:45 -0700

    stack complete

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,43 +1,103 @@
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
+        self.append = self.add_to_tail
+        self.pop = self.remove_tail
 
     def __repr__(self):
         node = self.head
         nodes = []
         while node is not None:
-            nodes.append(node.data)
+            nodes.append(node.value)
             node = node.next
         nodes.append("None")
         return " -> ".join(nodes)
 
-    def add_start(self, item):
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __len__(self):
+        count = 0
+        for n in self:
+            count += 1
+        return count
+
+    def add_to_head(self, item):
         n = Node(item)
         n.next = self.head
         self.head = n
+        if self.head.next is None:
+            self.tail = n
 
-    def add_end(self, item):
+    def add_to_tail(self, item):
         n = Node(item)
         if self.head is None:
             self.head = n
+            self.tail = n
             n.next = None
             return
         current_node = self.head
         while current_node.next is not None:
             current_node = current_node.next
-            continue
         current_node.next = n
         n.next = None
+        self.tail = n
+
+    def remove_tail(self):
+        if self.head is None:
+            return None
+        if self.head.next is None:
+            removed = self.head
+            self.head = None
+            self.tail = None
+            return removed.value
+        current_node = self.head
+        previous_node = None
+        while current_node.next is not None:
+            previous_node = current_node
+            current_node = current_node.next
+        previous_node.next = None
+        removed = self.tail
+        self.tail = previous_node
+        return removed.value
+
+    def remove_head(self):
+        if self.head is None:
+            return None
+        removed = self.head
+        if self.head.next is None:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+        return removed.value
+
+    def get_max(self):
+        if self.head is None:
+            return None
+        max_num = 0
+        for n in self:
+            if n.value > max_num:
+                max_num = n.value
+        return max_num
+
+    def contains(self, item):
+        for n in self:
+            if n.value == item:
+                return True
+        return False
+
 
 class Node:
     def __init__(self, data):
-        self.data = data
+        self.value = data
         self.next = None
 
-ll = LinkedList()
-print(ll)
-ll.add_start("Nina")
-ll.add_start("Tiger")
-ll.add_start("Leila")
-ll.add_end("John")
-print(ll)
+    def __repr__(self):
+        return self.value
+
+
--- stack/stack.py
+++ stack/stack.py
@@ -10,11 +10,118 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.append = self.add_to_tail
+        self.pop = self.remove_tail
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.value)
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __len__(self):
+        count = 0
+        for n in self:
+            count += 1
+        return count
+
+    def add_to_head(self, item):
+        n = Node(item)
+        n.next = self.head
+        self.head = n
+        if self.head.next is None:
+            self.tail = n
+
+    def add_to_tail(self, item):
+        n = Node(item)
+        if self.head is None:
+            self.head = n
+            self.tail = n
+            n.next = None
+            return
+        current_node = self.head
+        while current_node.next is not None:
+            current_node = current_node.next
+        current_node.next = n
+        n.next = None
+        self.tail = n
+
+    def remove_tail(self):
+        if self.head is None:
+            return None
+        if self.head.next is None:
+            removed = self.head
+            self.head = None
+            self.tail = None
+            return removed.value
+        current_node = self.head
+        previous_node = None
+        while current_node.next is not None:
+            previous_node = current_node
+            current_node = current_node.next
+        previous_node.next = None
+        removed = self.tail
+        self.tail = previous_node
+        return removed.value
+
+    def remove_head(self):
+        if self.head is None:
+            return None
+        removed = self.head
+        if self.head.next is None:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+        return removed.value
+
+    def get_max(self):
+        if self.head is None:
+            return None
+        max_num = 0
+        for n in self:
+            if n.value > max_num:
+                max_num = n.value
+        return max_num
+
+    def contains(self, item):
+        for n in self:
+            if n.value == item:
+                return True
+        return False
+
+
+class Node:
+    def __init__(self, data):
+        self.value = data
+        self.next = None
+
+    def __repr__(self):
+        return self.value
+
+
+
 class Stack:
     def __init__(self):
         self.size = 0
         self.storage = []
-
+        # self.storage = LinkedList()
+   
     def __len__(self):
         return len(self.storage)
 
@@ -22,15 +129,12 @@ class Stack:
         self.storage.append(value)
 
     def pop(self):
-        self.storage.pop()
+        try:
+            return self.storage.pop()
+        except IndexError:
+            return None
 
 
 
-s = Stack()
-s.push("Nina")
-print(len(s))
-s.push("Tiger")
-print(len(s))
-s.pop()
-print(len(s))
+
 
